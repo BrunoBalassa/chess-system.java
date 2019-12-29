9cb99ac9af9a7d17ec8decdc56b0ad6539818b8b
unknown <bno.balassa@gmail.com>
2019-12-29 19:56:19 -0300

    Starting

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,11 @@
 package com.company;
 
+import boardgame.Board;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Board board = new Board(8, 8);
     }
+
 }
